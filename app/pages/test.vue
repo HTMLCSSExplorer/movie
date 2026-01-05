49e6f888df5caae7c4e09a1e4b7e5fa7a93f7429
JSExplorers <htmlcssexplorer@gmail.com>
2026-01-04 23:20:39 -0600

    added media types

--- app/pages/test.vue
+++ app/pages/test.vue
@@ -1,9 +1,36 @@
 <template>
   <div>
-    <h1>..</h1>
+    <button @click="addId">+</button>
+    <UiLoadAnime v-if="pending" />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  const id = ref(0);
+  const { data, pending } = useAsyncData(
+    () => `id-${id.value}`,
+    () => {
+      return $fetch("/api/test");
+    },
+    {
+      watch: [id],
+
+      default() {
+        "def";
+      },
+    }
+  );
+
+  watchEffect(() => {
+    if (!pending.value) {
+      console.log(data.value);
+    }
+  });
+
+  const addId = () => {
+    id.value++;
+    console.log(id.value);
+  };
+</script>
 
 <style scoped></style>
---
+++ shared/types/media.ts
@@ -0,0 +1,55 @@
+import type { CountryCode } from "./countryCodes";
+import type { LanguageCode } from "./languageCodes";
+
+//-------------------------
+// ******Media Items*******
+//-------------------------
+export type MediaType = "movie" | "tv" | "person" | "kirekhar";
+export type MediaItem = MovieMedia | TvMedia | PersonMedia;
+
+// ******Base Media*******
+
+export type BaseMedia = {
+  adult?: boolean;
+  backdrop_path?: string;
+  genre_ids: number[];
+  id: number;
+  original_language: LanguageCode;
+  overview: string;
+  poster_path?: string;
+  popularity: number;
+  vote_average: number;
+  vote_count: number;
+};
+
+// ******Movie Media*******
+export interface MovieMedia extends BaseMedia {
+  original_title: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  media_tpe?: "movie";
+}
+
+// ******Tv Show Media*******
+export interface TvMedia extends BaseMedia {
+  first_air_date: string;
+  name: string;
+  origin_country: CountryCode[];
+  original_name: string;
+  media_tpe?: "tv";
+}
+
+// ******Person Media*******
+export interface PersonMedia {
+  media_tpe?: "person";
+  adult: boolean;
+  gender: number;
+  id: number;
+  known_for_department: string;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path?: string;
+  known_for: MediaItem[];
+}
